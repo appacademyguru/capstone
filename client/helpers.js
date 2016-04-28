6e55abd0f59cfdb4b3e97c3b6d6d4fd3b6e6eb6e
Megan McCreight <appacademy.megan@gmail.com>
2016-04-28 12:04:50 -0700

    styling results page

--- client/helpers.js
+++ client/helpers.js
@@ -9,16 +9,45 @@ Template.registerHelper("getInfo", function () {
     }).fetch();
     var cityMatch;
     console.log(cities);
-    
+
     for (var i = 0; i < cities.length; i++) {
         console.log("LOL!");
         if (cities[i].city == cityVar) {
             console.log("MATCH!");
             cityMatch = cities[i].city;
-//            Session.set('Cities', cities[i]);
-            return cityMatch;
+
+            break;
         }
     }
-    
-    return "why";
+    Session.set('Cities', cities[i]);
+});
+
+Template.registerHelper("cityName", function () {
+    var userCity = Session.get("Cities");
+    return userCity.city;
+});
+
+Template.registerHelper("gas", function () {
+    var userCity = Session.get("Cities");
+    return userCity.gas;
+});
+Template.registerHelper("bus", function () {
+    var userCity = Session.get("Cities");
+    return userCity.bus;
+});
+Template.registerHelper("onebedapt", function () {
+    var userCity = Session.get("Cities");
+    return userCity.onebedapt;
+});
+Template.registerHelper("salary", function () {
+    var userCity = Session.get("Cities");
+    return userCity.salary;
+});
+Template.registerHelper("electricity", function () {
+    var userCity = Session.get("Cities");
+    return userCity.electricity;
+});
+Template.registerHelper("water", function () {
+    var userCity = Session.get("Cities");
+    return userCity.water;
 });
